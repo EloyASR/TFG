dc53cedc563946ac46ad45443b1da1a72cdd913e
UO271588 <UO271588@uniovi.es>
2023-03-03 19:50:23 +0100

    Cambio ENV

--- webapp/src/services/loginService.js
+++ webapp/src/services/loginService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
-const url = 'http://localhost:8080/api/login'
+const url = process.env.REACT_APP_API_URI || 'http://localhost:8080/api/login'
 
 const loginService = {
 
