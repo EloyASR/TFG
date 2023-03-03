ada96c724ab45a8806334f54eebd5c4605e13a84
UO271588 <UO271588@uniovi.es>
2023-03-03 20:05:16 +0100

    Prueba

--- webapp/server.js
+++ webapp/server.js
@@ -1,7 +1,5 @@
 var express = require('express')
 
-export const baseurl =  'http://localhost:5000/api'
-
 var app = express()
 var port =  process.env.PORT || 3000
 app.use(express.static('build'))
