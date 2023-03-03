11299cc7537508b421d8bd5b7979c16ae5a4247a
UO271588 <UO271588@uniovi.es>
2023-03-03 20:18:43 +0100

    Prueba

--- webapp/src/services/loginService.js
+++ webapp/src/services/loginService.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
-const baseurl =  'http://localhost:5000/api'
+const baseurl = process.env.REACT_APP_API_URI || 'http://localhost:5000/api'
+
 let url = baseurl + '/login'
 
 const loginService = {
